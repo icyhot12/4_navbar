a46ee5a0c1eda660c15a67889ce9f1abb6968f7a
icyhot12 <rmierniczek1@gmail.com>
2022-09-19 16:00:22 +0200

    logic added

--- app.js
+++ app.js
@@ -1,12 +1,8 @@
-const menuButton = document.getElementById('menu');
-const smallMenu = document.querySelector('.menu-small-screens');
+const menuToggleButton = document.getElementById('menu');
+const smallMenu = document.querySelector('.menu-small-screens'); 
 
-menuButton.addEventListener('click', function () {
-    menuButton.style.transform = "rotate(90deg)";
-    smallMenu.style.display = "flex";
-    smallMenu.style.backgroundColor = "white";
-    return !menuDisplay;
-})
+function navbarToggle() {
+    smallMenu.classList.toggle('menu-small-screens-toggle');
+}
 
-
-// HOW TO USE TOGGLE??
+menuToggleButton.addEventListener('click',navbarToggle);
