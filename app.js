e29c1287dcd66f1cc6b3b8d7cd8195b59a872946
Rafał Mierniczek <rmierniczek1@gmail.com>
2022-09-19 14:56:17 +0200

    toggle function ?

--- app.js
+++ app.js
@@ -1,5 +1,12 @@
 const menuButton = document.getElementById('menu');
+const smallMenu = document.querySelector('.menu-small-screens');
 
-menuButton.addEventListener('click', function() {
+menuButton.addEventListener('click', function () {
     menuButton.style.transform = "rotate(90deg)";
+    smallMenu.style.display = "flex";
+    smallMenu.style.backgroundColor = "white";
+    return !menuDisplay;
 })
+
+
+// HOW TO USE TOGGLE??
